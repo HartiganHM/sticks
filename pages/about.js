934bf59792ac5262190ff46e2c404f51d35a1070
Hugh Hartigan <hartigan.hm@gmail.com>
2022-09-17 10:14:20 -0600

    chore: Update copy content

--- pages/about.js
+++ pages/about.js
@@ -1,2 +1,2 @@
-const About = () => <div>About us</div>;
+const About = () => <div>Ur cute 😘</div>;
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 const Index = () => (
   <div>
-    Hello World.{' '}
+    Hi Colin 🤘{' '}
     <Link href="/about">
       <a>About</a>
     </Link>
