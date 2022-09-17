6b3a6d335625531db970edb7b5cd30b2abcab590
Hugh Hartigan <hartigan.hm@gmail.com>
2022-09-17 10:27:55 -0600

    build: Add artists GET and return mock resposne

--- pages/api/artists.js
+++ pages/api/artists.js
@@ -1,5 +1,7 @@
+import mockArtistsResponse from './mocks/mockArtistsResponse';
+
 export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json({ name: "John Doe" });
+  if (req.method === 'GET') {
+    res.status(200).json(mockArtistsResponse);
   }
 }
---
+++ pages/api/mocks/mockArtistsResponse.js
@@ -0,0 +1,17 @@
+const mockArtistsResponse = [
+  {
+    artist: 'Aenigmatum',
+    album: 'Consecrate',
+    rating: 4,
+    notes: `Rhythm section absolutely dominates this record. Drums are super dynamic and aggressive, tons of fills and intricate phrasings. Bass is up in the front of the mix, lots of intricate lines. I was originally skeptical because I don't usually like albums where the guitar feels thin, but it works here, and there are some great riffs and lead lines mixed in. Guitars on Fracturing Proclivity are wild.`,
+    year: 2021,
+    subGenre: 'Deathy Black',
+    standouts: [
+      'Undaunted Hereafter',
+      'Disenthralled',
+      'Fracturing Proclivity',
+    ],
+  },
+];
+
+export default mockArtistsResponse;
